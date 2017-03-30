021ec8ad679208dfbcd6559a27240aafbf09d40e
Charles <cer@a2c3.co>
2017-03-30 03:15:09 -0700

    fixed

---
+++ wtplayer/index.php
@@ -0,0 +1,27 @@
+<html>
+    <body>
+       
+    </body>
+    <script src="/addon/wtplayer/webtorrent.min.js"></script>
+    <script>
+        var client = new WebTorrent()
+        var torrentId = '<?php if(isset($_GET["magnet"])) { echo str_replace("&amp;","&",base64_decode($_GET["magnet"])); } ?>';
+        client.add(torrentId, function (torrent) {
+          var file = torrent.files[0];
+          file.appendTo('body');
+        });
+    </script>
+    <style>
+        body {
+            margin: 0px;
+            padding: 0px;
+            background: #000;
+            color: #fff;
+        }
+        video {
+            height: 300px;
+            width:100%;
+        }
+    </style>
+    
+</html>
