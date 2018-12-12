d860f9d8a0c379e55a96535605a21163c3c193e7
demitas <demitas.ace@gmail.com>
2018-12-12 09:31:54 -0800

    - Fix failure to play vid when files[0] is not the movie - Added support for multiple videos in a post - Replace magnet string instead of appending to it

--- wtplayer/wtplayer.php
+++ wtplayer/wtplayer.php
@@ -3,7 +3,7 @@
  *
  * Name: Webtorrent player
  * Description: A Webtorrent player for hubzilla. Alpha. Unstable. For testing.
- * Version: 0.2
+ * Version: 0.3
  * Depends: Core
  * Recommends: None
  * Category: Torrents
@@ -22,45 +22,65 @@ function wtplayer_unload(){
 }
 
 function wtplayer_prepare_body(&$a,&$b) {
-    /*
-        Please note: this is not a finished work. It was a "speed coding"
-        thing. I will make it more clean overtime. If you wish to help me,
-        just do a pull request at https://github.com/dfstorm/wtplayer-hubzilla
-    
-    */
-    
     $sStartTag = '[webtorrent]';
     $sEndTag = '[/webtorrent]';
     $sBody = $b['html'];
-    $uId = uniqid();
-    
-    if(strpos($b['html'],$sStartTag) === false) {
+
+
+    $iLastStartIndex = strpos($b['html'], $sStartTag);
+    if($iLastStartIndex === false) {
         return;
     }
-    
-	$iStart = strpos($sBody,$sStartTag);
-	$iEnd = strpos($sBody,$sEndTag,$iStart+1);
-    
-    $magnetLink = substr($sBody,$iStart+strlen($sStartTag),$iEnd-(strlen($sEndTag)-1));
-    $encodedMagnetLink = base64_encode($magnetLink);
 
-   $sPlayerblock = <<<HTMLRENDER
-   <div id="wtplayer_{$uId}" style="background:black; color:#fff; height: 300px; width: 100%; text-align:center;">
-    <br /><br />Magnet link detected.
-    <a onclick="wtplayerActivate_{$uId}();" class="">launch it</a>
-    <br />Powerded by webtorrent.io<br /><br />
-    {$magnetLink}
-   </div>
-   <script>
-    function wtplayerActivate_{$uId}() {
-        $('#wtplayer_{$uId}').html('<iframe src="/addon/wtplayer/view/tpl/index.php?magnet={$encodedMagnetLink}" style="width: 100%;height: 400px; border:none;"></iframe>');
+
+    while($iLastStartIndex !== false)
+    {
+        $iStartIdx = $iLastStartIndex;
+        $iEndIdx = strpos($sBody, $sEndTag, $iStartIdx + 1);
+        $magnetLink = substr($sBody,$iStartIdx+strlen($sStartTag),$iEndIdx-(strlen($sEndTag)-1));
+
+        if(strpos($magnetLink, $sStartTag) !== false)
+        {
+            // nested webtorrent tags, return gracefully
+            return;
+        }
+
+        $encodedMagnetLink = base64_encode($magnetLink);
+        $uId = uniqid();
+
+        $sReplacement = wtplayer_get_replacement_block($uId, $encodedMagnetLink);
+
+        $sStartString = "";
+        if($iStartIdx > 0)
+        {
+            $sStartString = substr($sBody, 0, $iStartIdx);
+        }
+        $sEndString = substr($sBody, $iEndIdx + strlen($sEndTag));
+
+        $sBody = $sStartString . $sReplacement . $sEndString;
+        $iLastStartIndex = strpos($sBody, $sStartTag, $iLastStartIndex+1);
     }
-   </script>
-   
-   
+
+    $b['html'] = $sBody;
+}
+
+function wtplayer_get_replacement_block($uId, $encodedLink)
+{
+    $sPlayerblock = <<<HTMLRENDER
+        <div id="wtplayer_{$uId}" style="background:black; color:#fff; height: 300px; width: 100%; text-align:center;">
+            <p>Magnet link detected. <a onclick="wtplayerActivate_{$uId}();" class="">Launch it!</a><br/>
+            Powered by webtorrent.io</p>
+       </div>
+       <script>
+        function wtplayerActivate_{$uId}() {
+            var htmlString = '<iframe src="/addon/wtplayer/view/tpl/index.php?magnet={$encodedLink}&uId={$uId}" ' +
+                'style="width: 100%; height: 400px; border:none;"></iframe>';
+            $('#wtplayer_{$uId}').html(htmlString);
+        }
+       </script>
 HTMLRENDER;
-    
-    $b['html'] .= $sPlayerblock;    
+
+    return $sPlayerblock;
 }
 
 
